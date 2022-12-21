4e6c581c1038d7abacde72241bf04819e2998023
Callie Stoscup <calliestoscup@gmail.com>
2022-12-21 11:43:44 -0500

    Loaded example data into database

---
+++ server/loadFeatures.js
@@ -0,0 +1,28 @@
+const { Feature } = require('./database.js');
+const lineByLine = require('n-readlines');
+const liner = new lineByLine('./server/data/features1.csv');
+
+let line = liner.next();
+const data = [];
+
+while (line = liner.next()) {
+  const row = line.toString('ascii').split(/(?!\B"[^"]*),(?![^"]*"\B)/);
+  data.push({
+    id: Number(row[0]),
+    ProductId: Number(row[1]),
+    feature: row[2].replaceAll('"', ''),
+    value: row[3].replaceAll('"', ''),
+  })
+}
+
+console.log(data);
+
+async function saveData() {
+  let chunkSize = 100000;
+  for (let i = 0; i < data.length; i+= chunkSize) {
+    let chunk = data.slice(i, i + chunkSize);
+    await Feature.bulkCreate(chunk);
+  }
+}
+
+saveData();
